0b76bb3d802605299e2259bd52223e9dff0f46ba
Kristina Valitova <valitovakris@gmail.com>
2024-01-09 22:36:09 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrise = 13676;
+        int milePrise = 20;
+        int miles = ticketPrise / milePrise;
+
+        System.out.println( miles + " " + "миль будет начислено за покупку одного билета");
+    }
+}
